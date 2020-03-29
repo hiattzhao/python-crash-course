8125dd940e084fdde68456feedefbfa4594d653a
Hiatt Zhao <hiattzhao@gmail.com>
2020-03-29 14:17:14 -0400

    create list comprehensions

---
+++ list_comprehension.py
@@ -0,0 +1,7 @@
+#list comprehension
+odd_numbers = [value for value in range(1,21,2)]
+print(odd_numbers)
+
+#cubes
+cubes = [value**3 for value in range(1,21)]
+print(cubes)
--- range.py
+++ range.py
@@ -7,11 +7,3 @@ print(numbers)
 print(min(numbers))
 print(max(numbers))
 print(sum(numbers))
-
-#list comprehension
-odd_numbers = [value for value in range(1,21,2)]
-print(odd_numbers)
-
-#cubes
-cubes = [value**3 for value in range(1,21)]
-print(cubes)
