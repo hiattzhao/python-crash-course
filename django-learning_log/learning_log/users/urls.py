f1e09876de4eda9b36a338edb943dc524043dec8
Hiatt Zhao <hiattzhao@gmail.com>
2021-02-23 14:38:32 -0500

    Finish chapter 19

--- django-learning_log/learning_log/users/urls.py
+++ django-learning_log/learning_log/users/urls.py
@@ -10,5 +10,8 @@ from . import views
 urlpatterns = [
   # Login page
   path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+  # Logout page
+  path('logout/', views.logout_view, name='logout'),
+  # Registration page
   path('register/', views.register, name='register'),
 ]
